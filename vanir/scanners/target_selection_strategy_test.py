880d1879f6d89d30588f65ab9873d10d8066a57c
Shivam Mishra <shivam.mishra.test.email@gmail.com>
2026-09-18 06:59:15 +0000

    Test commit 3
    Commit contains few code formatting updates.

--- vanir/scanners/target_selection_strategy_test.py
+++ vanir/scanners/target_selection_strategy_test.py
@@ -152,12 +152,14 @@ class TargetSelectionStrategyTest(absltest.TestCase):
 
   # Adding new test for validating PR intake.
   def test_exact_path_match_strategy_from_file_set(self):
-    logging.info('Running test_exact_path_match_strategy_from_file_set in vanir/scanners/.')
+    logging.info(
+        'Running test_exact_path_match_strategy_from_file_set in'
+        ' vanir/scanners/.'
+    )
     file_set = {'exact_match1.c', 'foo/exact_match2.c', 'no_match.c'}
-    to_scan, skipped = (
-        target_selection_strategy.Strategy.EXACT_PATH_MATCH.get_target_files_from_file_set(
-            file_set, self._mock_sign_bundle
-        )
+    strategy = target_selection_strategy.Strategy.EXACT_PATH_MATCH
+    to_scan, skipped = strategy.get_target_files_from_file_set(
+        file_set, self._mock_sign_bundle
     )
     self.assertEqual(to_scan, {'exact_match1.c', 'foo/exact_match2.c'})
     self.assertEqual(skipped, 1)
